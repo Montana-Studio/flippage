cb44ccd94d70c79126c990557e7d6c930ff0e6e9
argkont <kohacaelo@gmail.com>
2016-05-18 10:06:28 -0400

    first commit

---
+++ app/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>FlipBook</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/flipbook.css">
+    
+    <meta name="description" content="Una revista internacional"/>
+    
+    <meta property="og:locale" content="es_ES" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Revista COSAS - Una revista internacional" />
+    <meta property="og:description" content="Una revista internacional" />
+    <meta property="og:url" content="http://cosas.com" />
+    <meta property="og:site_name" content="Revista COSAS" />
+    
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:description" content="Una revista internacional" />
+    <meta name="twitter:title" content="Revista COSAS - Una revista internacional" />
+</head>
+<body>
+   
+   <a href="javascript:fbShare('http://cosas.com/anna-queen-of-tribeca/', 'ANNA QUEEN OF TRIBECA // FESTIVAL CINE EN NUEVA YORK', 'ANNA QUEEN OF TRIBECA // FESTIVAL CINE EN NUEVA YORK', 'http://cosas.com/anna-queen-of-tribeca/', 520, 350)">facebook</a>
+            
+    <a href="javascript:twShare('http://cosas.com/anna-queen-of-tribeca/', 'ANNA QUEEN OF TRIBECA // FESTIVAL CINE EN NUEVA YORK - vía: @revistacosas', 520, 350)">twitter</a>
+
+    <a href="javascript:piShare('http://cosas.com/anna-queen-of-tribeca/', 'ANNA QUEEN OF TRIBECA // FESTIVAL CINE EN NUEVA YORK', 'http://cosas.com/wp-content/uploads/2016/05/1-Anna-Wintour-GettyImages-521028634.jpg', '', 520, 350)">pinteres</a>
+
+    <a target="_blank" href="https://plus.google.com/share?url=http://cosas.com/anna-queen-of-tribeca/" onclick="window.open('https://plus.google.com/share?url=http://cosas.com/anna-queen-of-tribeca/','gplusshare','width=600,height=400,left='+(screen.availWidth/2-225)+',top='+(screen.availHeight/2-150)+'');return false;">google plus</a>
+
+    <div id="book">
+        <div class="cover"><img src="img/part-1.jpg" alt=""/></div>
+    </div>
+
+    <div id="controls">
+        <label for="page-number">Page:</label> <input type="text" size="3" id="page-number"> of <span id="number-pages"></span>
+    </div>
+    
+    <script src="https://code.jquery.com/jquery-2.2.3.min.js"></script>
+    <script async src="js/turn.min.js"></script>
+    <script async src="js/flipbook.min.js"></script>
+</body>
+</html>
---
+++ prod/js/scripts.js
@@ -0,0 +1,71 @@
+jQuery(document).ready(function($){
+    var numberOfPages = 46; 
+	// Adds the pages that the book will need
+	function addPage(page, book) {
+		// 	First check if the page is already in the book
+		if (!book.turn('hasPage', page)) {
+			// Create an element for this page
+			var element = $('<div />', {'class': 'page '+((page%2===0) ? 'odd' : 'even'), 'id': 'page-'+page}).html('<i class="loader"></i>');
+			// If not then add the page
+			book.turn('addPage', element, page);
+			// Let's assum that the data is comming from the server and the request takes 1s.
+			setTimeout(function(){
+					element.html('<div class="data"><img src="img/part-'+page+'.jpg"/></div>');
+			}, 1000);
+		}
+	}
+    
+    $('#book').turn('resize',{
+        pages: numberOfPages,
+        
+        when: {
+            turning: function(e, page, view) {
+                // Gets the range of pages that the book needs right now
+                var range = $(this).turn('range', page);
+                // Check if each page is within the book
+                for (page = range[0]; page<=range[1]; page++){
+                    addPage(page, $(this));
+                }
+            },
+            turned: function(e, page) {
+                $('#page-number').val(page);
+            }
+        }
+    });
+    
+    $('#number-pages').html(numberOfPages);
+    
+    $('#page-number').keydown(function(e){
+        if (e.keyCode===13){
+            $('#book').turn('page', $('#page-number').val());
+        }
+
+    });
+    
+    $(window).bind('keydown', function(e){
+		if (e.target && e.target.tagName.toLowerCase()!=='input'){
+			if (e.keyCode===37){
+				$('#book').turn('previous');
+    }else if (e.keyCode===39){
+				$('#book').turn('next');
+            }
+        }
+	});
+    
+}); 
+
+function fbShare(url, title, descr, image, winWidth, winHeight) {
+    var winTop = (screen.height / 2) - (winHeight / 2);
+    var winLeft = (screen.width / 2) - (winWidth / 2);
+    window.open('http://www.facebook.com/sharer.php?s=100&p[title]=' + title + '&p[summary]=' + descr + '&p[url]=' + url + '&p[images][0]=' + image, 'sharer', 'top=' + winTop + ',left=' + winLeft + ',toolbar=0,status=0,width=' + winWidth + ',height=' + winHeight);
+}
+function twShare(url, title, winWidth, winHeight) {
+    var winTop = (screen.height / 2) - (winHeight / 2);
+    var winLeft = (screen.width / 2) - (winWidth / 2);
+    window.open('http://twitter.com/share?url=' + url + '&text=' + title, '', 'left=0,top=0,width=' + winWidth + ',height=' + winHeight + ',personalbar=0,toolbar=0,scrollbars=0,resizable=0');
+}
+function piShare(url, title, image, winWidth, winHeight) {
+    var winTop = (screen.height / 2) - (winHeight / 2);
+    var winLeft = (screen.width / 2) - (winWidth / 2);
+    window.open('https://pinterest.com/pin/create/button/?url=' + url + '&media=' + image + '&description=' + title +'');
+}
